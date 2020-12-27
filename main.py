576c25b3c09a1cef48202197cd0360d6c300cc19
Mustafa badr <36304069+moustafaBadrr@users.noreply.github.com>
2020-12-27 19:49:55 +0200

    Update main.py
    FinalVersion

--- main.py
+++ main.py
@@ -1,71 +1,75 @@
-def getValue(points,X):
-    #print(points)
-    a=(points[1][1]-points[0][1])/(points[1][0]-points[0][0])
-    b=points[1][1]-a*points[1][0]
-    #print(a," *x+ ",b)
-    return a*X+b
-def linesEquations(inputs): #mina
-    project_funding_sets=[[0,0,10,30],[10,30,40,60],[40,60,70,90],[70,90,100,100]]
-    project_funding_states=["very low","low","medium","high"]
-    team_experience_level_sets=[[0,15,30], [15,30,45], [30,60,60]]
-    team_experience_level_states=["beginner", "intermediate", "expert"]
-    #input=[50,40]
-    lines4Shape=[0,1,1,0]
-    lines3Shape = [0, 1, 0]
+def get_value(points, x):
+    # print(points)
+    a = (points[1][1] - points[0][1]) / (points[1][0] - points[0][0])
+    b = points[1][1] - a * points[1][0]
+    # print(a," *x+ ",b)
+    return a * x + b
+
+
+def lines_equations(inputs):  # mina
+    project_funding_sets = [[0, 0, 10, 30], [10, 30, 40, 60], [40, 60, 70, 90], [70, 90, 100, 100]]
+    project_funding_states = ["very low", "low", "medium", "high"]
+    team_experience_level_sets = [[0, 15, 30], [15, 30, 45], [30, 60, 60]]
+    team_experience_level_states = ["beginner", "intermediate", "expert"]
+    # input=[50,40]
+    lines4_shape = [0, 1, 1, 0]
+    lines3_shape = [0, 1, 0]
     points1 = []
-    state1=[]
+    state1 = []
     for i in range(len(project_funding_sets)):
-        state=project_funding_states[i]
-        if(min(project_funding_sets[i])<inputs[0] and max(project_funding_sets[i])>inputs[0]):
-            #print(project_funding_sets[i])
+        state = project_funding_states[i]
+        if (min(project_funding_sets[i]) < inputs[0]) and max(project_funding_sets[i]) > inputs[0]:
+            # print(project_funding_sets[i])
             state1.append(state)
             for j in range(len(project_funding_sets[i])):
-                if project_funding_sets[i][j]>inputs[0]:
-                    line=[[project_funding_sets[i][j-1], lines4Shape[j-1]],[project_funding_sets[i][j],lines4Shape[j]]]
+                if project_funding_sets[i][j] > inputs[0]:
+                    line = [[project_funding_sets[i][j - 1], lines4_shape[j - 1]],
+                            [project_funding_sets[i][j], lines4_shape[j]]]
                     points1.append(line)
                     break
-    res1=[]
+    res1 = []
     for i in range(len(points1)):
-        y=getValue(points1[i],inputs[0])
+        y = get_value(points1[i], inputs[0])
         res1.append(y)
-    #print(points1)
+    # print(points1)
     points2 = []
     state2 = []
     for i in range(len(team_experience_level_sets)):
         state = team_experience_level_states[i]
-        if (min(team_experience_level_sets[i]) < inputs[1] and max(team_experience_level_sets[i]) > inputs[1]):
-            #print(team_experience_level_sets[i])
+        if (min(team_experience_level_sets[i]) < inputs[1]) and max(team_experience_level_sets[i]) > inputs[1]:
+            # print(team_experience_level_sets[i])
             state2.append(state)
             for j in range(len(team_experience_level_sets[i])):
                 if team_experience_level_sets[i][j] > inputs[1]:
-                    line=[[team_experience_level_sets[i][j-1],lines3Shape[j-1]],[team_experience_level_sets[i][j],lines3Shape[j]]]
+                    line = [[team_experience_level_sets[i][j - 1], lines3_shape[j - 1]],
+                            [team_experience_level_sets[i][j], lines3_shape[j]]]
                     points2.append(line)
                     break
-    res2=[]
+    res2 = []
     for i in range(len(points2)):
-        y=getValue(points2[i],inputs[1])
+        y = get_value(points2[i], inputs[1])
         res2.append(y)
-    states=[state1,state2]
-    fuzzyValues=[res1,res2]
-    #print(points2)
-    #print(fuzzyValues)
-    #print(states)
+    states = [state1, state2]
+    fuzzy_values = [res1, res2]
+    # print(points2)
+    # print(fuzzyValues)
+    # print(states)
 
-    return fuzzyValues, states
+    return fuzzy_values, states
 
 
 def get_index_of_value(value, arr_of_value):  # Mustafa
-    for i in range (0, len(arr_of_value)):
+    for i in range(0, len(arr_of_value)):
         if arr_of_value[i] == value:
             return i
     return -1
 
 
-def rules (fuzzy_values, states):  # mustafa
-
+def rules(fuzzy_values, states):  # mustafa
+    rule1_value, rule2_value, rule3_value, rule4_value = 0, 0, 0, 0
     # First Rule . If project_funding is high or team_experience_level is expert then risk is low
     state1_risk = "low"
-    if "high" in states[0] and "expert" in states [1]:
+    if "high" in states[0] and "expert" in states[1]:
         value_of_high = fuzzy_values[0][get_index_of_value("high", states[0])]
         value_of_expert = fuzzy_values[1][get_index_of_value("expert", states[1])]
         rule1_value = max(value_of_high, value_of_expert)
@@ -108,67 +112,69 @@ def rules (fuzzy_values, states):  # mustafa
     return [rule1_value, rule2_value, rule3_value, rule4_value], [state1_risk, state2_risk, state3_risk, state4_risk]
 
 
+def calc_the_result(rules_values_res, rules_states_res):  # peter
 
-def calcTheResult(rulesValuesRes, rulesStatesRes):  # peter
-
-    riskSets = [[0, 25, 50], [25, 50, 75], [50, 100, 100]]
-    riskStates = ["high", "normal", "low"]
-    centroid_high = sum(riskSets[0]) / len(riskSets[0])
-    centroid_normal = sum(riskSets[1]) / len(riskSets[1])
-    centroid_low = sum(riskSets[2]) / len(riskSets[2])
+    risk_sets = [[0, 25, 50], [25, 50, 75], [50, 100, 100]]
+    centroid_high = sum(risk_sets[0]) / len(risk_sets[0])
+    centroid_normal = sum(risk_sets[1]) / len(risk_sets[1])
+    centroid_low = sum(risk_sets[2]) / len(risk_sets[2])
     numerator = []
-    for i in range(len(rulesValuesRes)):
-        if rulesStatesRes[i] == 'low':
-            numerator.append(rulesValuesRes[i] * centroid_low)
-        elif rulesStatesRes[i] == 'normal':
-            numerator.append(rulesValuesRes[i] * centroid_normal)
+    for i in range(len(rules_values_res)):
+        if rules_states_res[i] == 'low':
+            numerator.append(rules_values_res[i] * centroid_low)
+        elif rules_states_res[i] == 'normal':
+            numerator.append(rules_values_res[i] * centroid_normal)
         else:
-            numerator.append(rulesValuesRes[i] * centroid_high)
-    risk = sum(numerator) / sum(rulesValuesRes)
+            numerator.append(rules_values_res[i] * centroid_high)
+    risk = sum(numerator) / sum(rules_values_res)
     possible_states = []
     points = []
-    lines3Shape = [0, 1, 0]
-    for i in range(len(riskSets)):
-        if min(riskSets[i]) < risk < max(riskSets[i]):
+    lines3_shape = [0, 1, 0]
+    for i in range(len(risk_sets)):
+        if min(risk_sets[i]) < risk < max(risk_sets[i]):
             if i == 0:
                 possible_states.append("high")
             elif i == 1:
                 possible_states.append("normal")
             else:
                 possible_states.append("low")
-            for j in range(len(riskSets[i])):
-                if riskSets[i][j] > risk:
-                    line = [[riskSets[i][j - 1], lines3Shape[j - 1]],
-                            [riskSets[i][j], lines3Shape[j]]]
+            for j in range(len(risk_sets[i])):
+                if risk_sets[i][j] > risk:
+                    line = [[risk_sets[i][j - 1], lines3_shape[j - 1]],
+                            [risk_sets[i][j], lines3_shape[j]]]
                     points.append(line)
                     break
 
     result = []
     for i in range(len(points)):
-        y = getValue(points[i], risk)
+        y = get_value(points[i], risk)
         result.append(y)
     max_intersect = max(result)
-    index = get_index_of_value(max_intersect,result)
+    index = get_index_of_value(max_intersect, result)
     result_state = possible_states[index]
-    return risk , result_state
+    return risk, result_state
+
 
 def fuzzy(inputs):
-    fuzzyValues,states=linesEquations(inputs)
-    rulesValuesRes,rulesStatesRes=rules(fuzzyValues, states)
-    risk,state=calcTheResult(rulesValuesRes, rulesStatesRes)
-    return risk,state
+    fuzzy_values, states = lines_equations(inputs)
+    rules_values_res, rules_states_res = rules(fuzzy_values, states)
+    risk, state = calc_the_result(rules_values_res, rules_states_res)
+    return risk, state
+
+
 def main():
-    variablesNum = int(input("Variables :"))
-    inputs=[]
-    for i in range(variablesNum):
+    variables_num = int(input("Variables :"))
+    inputs = []
+    for i in range(variables_num):
         if i == 0:
             x = int(input("Project Fund :"))
             inputs.append(x)
         elif i == 1:
             x = int(input("Experience Level :"))
             inputs.append(x)
-    risk, state=fuzzy(inputs)
-    print("risk =",risk)
+    risk, state = fuzzy(inputs)
+    print("risk =", risk)
     print("risk will be ", state)
-    
+
+
 main()
